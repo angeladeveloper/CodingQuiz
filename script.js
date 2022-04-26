9684d76ad5c02b56a7b693c0bf9cf14a39d014ee
Angela W <squittenplease@gmail.com>
2022-04-25 21:06:42 -0700

    update css and js

--- script.js
+++ script.js
@@ -1,37 +1,81 @@
-let highscores = 0;
-const questionArray = [
-    //Here I will make 
-    {
-    prompt: "What element is used to define the font used?\n(a) Font \n(b) Font-Name \n(c) Font-src  \n(d) Font-family",
-    answer: "d"
-    },
-    {
-    prompt: "What operand is used to show a range?\n(a) Like \n(b) []? \n(c) %  \n(d) () ",
-    answer: "b"        
-    },
-    {
-    prompt: "The [typeof] operator in JavaScript always returns a/an?\n(a) Object \n(b) Function \n(c) String  \n(d) Number ",
-    answer: "c" 
-    },
-    {
-    prompt: "What array method joins all array elements into a string?\n(a) strjoin() \n(b) stringcon() \n(c) concatenate()  \n(d) Join () ",
-    answer: "d"         
-    }
-];
+const startBtn = document.getElementById("startBtn")
+const questionElement =document.getElementById('questionEle');
 
-console.log("QuestionArray", questionArray);
+startBtn.addEventListener('click', startGame);
 
-for (let i = 0; i < questionArray.length; i++) {
-    const userResponse = window.prompt(questionArray[i].prompt);
-    if ( userResponse == questionArray[i].answer){
-        highscores++;
-        alert("Correct");
-        console.log('User response', userResponse);
-        
-    }else{
-        alert("Wrong");
-    }
-    
+//event listener to start game. 
+let currentIndex, shuffledQuestions;
+
+function startGame() {
+    console.log('Start Game');
+
+// I am shuffleing the questions
+    shuffledQuestions  = questionArray.sort(() => Math.random() - .5);
+    console.log('Shuffled questions;', shuffledQuestions);//shuffle the questions.   
+    currentIndex = 0;// start at the first question
+    showQuestion();
 }
 
-alert(`You got ${highscores} / ${questionArray.length}`);
+function showQuestion(question) {
+//I need to hide the start button
+    startBtn.classList.add('hide');
+//I need to show the questions. 
+    // questionElement.innerText = question.question; 
+
+//I need to know what questions are right. 
+}
+
+// function selectAnswer(){
+
+// }
+
+// function nextQuestion() {
+
+// }
+
+
+
+
+
+
+
+
+// let userScores = 0;
+const questionArray = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+          { text: '4', correct: true },
+          { text: '22', correct: false },
+          { text: 'Corn', correct: false },
+          { text: '3️⃣', correct: false }
+        ]
+      },
+      {
+        question: 'What is the best animal?',
+        answers: [
+          { text: 'Cat', correct: true },
+          { text: 'Dog', correct: true },
+          { text: 'Rats', correct: true },
+          { text: 'All animals are the best', correct: true }
+        ]
+      },
+      {
+        question: 'Is web development fun?',
+        answers: [
+          { text: 'Kinda', correct: false },
+          { text: 'YES!!!', correct: true },
+          { text: 'Um no', correct: false },
+          { text: 'IDK', correct: false }
+        ]
+      },
+      {
+        question: 'What is 4 * 2?',
+        answers: [
+          { text: '16', correct: false },
+          { text: '26', correct: false },
+          { text: '64', correct: false },
+          { text: '8', correct: true }
+        ]
+      }
+    ];
